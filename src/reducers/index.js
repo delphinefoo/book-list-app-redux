b2c4fe1507d327fe2fbe67788f2f2dcd5a32f274
Delphine Foo-Matkin <delphinef@gmail.com>
2016-01-26 08:28:16 -0800

    Added first reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
+// Any key that we provide in the object provided to
+// combineReducers ends up as a key in the global state
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
